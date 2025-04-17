35a6fe7a6e41b1d73f27e31c701817c86a266b09
412088_dellacqua <412088@tecnicatura.frc.utn.edu.ar>
2025-04-16 21:01:33 -0300

    prueba

--- nombre-del-proyecto/src/main/java/com/tuempresa/tuprojecto/App.java
+++ nombre-del-proyecto/src/main/java/com/tuempresa/tuprojecto/App.java
@@ -1,13 +1,50 @@
 package com.tuempresa.tuprojecto;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        int opcion = 0;
+        Scanner sc = new Scanner(System.in);
+
+        do {
+
+            System.out.println("Bienvenido, Por favor elija una opcion");
+            System.out.println("1. Validar contrasena segura");
+            System.out.println("2. Salir");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    String contrasena = sc.nextLine();
+                    System.out.println(validarContrasena(contrasena));
+                    break;
+
+                case 2:
+                    break;
+            }
+
+        }while( opcion != 2 );
+
     }
+
+
+    public static boolean validarContrasena(String contrasena){
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
+
+        if(contrasena.matches(regex)){
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
 }
